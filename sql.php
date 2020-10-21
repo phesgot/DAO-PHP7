5a3d2db9a84c399e716cfd242948c5b4b5cf06a1
Pedro Torres <phesgot001@gmail.com>
2020-10-21 17:34:03 -0300

    CLASSE SQL

---
+++ sql.php
@@ -0,0 +1,54 @@
+<?php 
+
+class Sql extends PDO{
+
+
+	private $conn;
+
+
+
+	public function __construct(){
+
+		$this->conn = new PDO("mysql:dbname=dbphp7;host=localhost", "root", "1a2b3c4d5e6f@A");
+	}
+
+
+
+	private function setParams($statment, $parameters = array()){
+		foreach ($parameters as $key => $value) {
+			$this->setParam($key, $value);
+		}
+	}
+
+
+
+	private function setParam($statment, $key, $value){
+		$statment->bindParam($key, $value);
+	}
+
+
+
+	public function query($rawQuerry, $params = array()){
+		$stmt = $this->conn->prepare($rawQuerry);
+
+		$this->setParams($stmt, $params);
+
+		$stmt->execute();
+
+		return $stmt; 
+	}
+
+
+
+	public function select($rawQuerry, $params = array()):array{
+		$stmt = $this->query($rawQuerry, $params);
+
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+	}
+}
+
+
+
+
+
+ ?>
